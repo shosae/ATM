5ec4a2ce30e1ccb5615426910ae95a320cd00f90
shosae <phone13324@gmail.com>
2025-07-20 18:22:03 +0900

    fix(account): Account 클래스에 __eq__ 메서드 추가로 객체 비교 오류 해결 #3
    메모리값 기준 비교에서 __eq__를 통해 계좌번호 기준 비교로 변경

--- app/domain/account.py
+++ app/domain/account.py
@@ -2,3 +2,8 @@ class Account:
     def __init__(self, acc_number: str, balance: int):
         self.acc_number = acc_number
         self.balance = balance
+    
+    def __eq__(self, other):
+        if isinstance(other, Account):
+            return self.acc_number == other.acc_number
+        return False
