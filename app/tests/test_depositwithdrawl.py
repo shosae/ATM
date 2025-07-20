c53f1fa97f5c4dcdba97096c33d8727563b952d3
shosae <phone13324@gmail.com>
2025-07-20 19:34:19 +0900

    test: fixture(setUp)로 ATM 객체, 카드, 계좌 초기화 통합 #3

--- app/tests/test_depositwithdrawl.py
+++ app/tests/test_depositwithdrawl.py
@@ -1,49 +1,31 @@
+import pytest
 from domain.card import Card
 from domain.account import Account
 from controller.atm_controller import ATMController
 
+@pytest.fixture
+def setUp():
+    controller = ATMController()
+    card = controller.find_card_by_number("1234")
+    acc = card.acc_list[0] 
+    return controller, card, acc
 
-def test_deposit():
-    """입금 테스트"""
-    atm_controller = ATMController()
-    card = atm_controller.find_card_by_number("1234")
-
-    # 카드 등록 여부 검증
-    assert atm_controller.is_registered_card(card)
-
-    # 2. PIN 인증
-    assert card.verify_pin("4321") is True
-    acc = card.acc_list[0]  # "11-11" 계좌
-
-    # 3. 계좌 연결 확인
-    accounts = atm_controller.get_accounts_by_card(card)
-    assert acc in accounts
-
-    # 4. 입금
-    atm_controller.deposit(card, acc, 40)
-
-    # 5. 입금 후 잔액 검증
+def test_deposit(setUp):
+    """정상 입금 테스트"""
+    controller, card, acc = setUp
+    controller.deposit(card, acc, 40)
     assert acc.balance == 70
 
     
-def test_withdraw():
-    """출금 테스트"""
-    atm_controller = ATMController()
-    card = atm_controller.find_card_by_number("1234")
-
-    # 카드 등록 여부 검증
-    assert atm_controller.is_registered_card(card)
-
-    # 2. PIN 인증
-    assert card.verify_pin("4321") is True
-    acc = card.acc_list[0]  # "11-11" 계좌
-
-    # 3. 계좌 연결 확인
-    accounts = atm_controller.get_accounts_by_card(card)
-    assert acc in accounts
-
-    # 4. 출금 실행
-    atm_controller.withdraw(card, acc, 20)
-
-    # 5. 출금 후 잔액 검증
+def test_withdraw(setUp):
+    """정상 출금 테스트"""
+    controller, card, acc = setUp
+    controller.withdraw(card, acc, 20)
     assert acc.balance == 10
+
+def test_withdraw_raises_error_when_balance_insufficient(setUp):
+    """비정상 출금 테스트"""
+    controller, card, acc = setUp
+    result = controller.withdraw(card, acc, 40)
+    assert result is None  
+    assert acc.balance == 30    
