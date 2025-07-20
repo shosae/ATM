b515a0ab494d4c35fd063557daf878f02183693f
shosae <phone13324@gmail.com>
2025-07-20 22:11:38 +0900

    refactor: 파라미터라이즈로 테스트 통합, Exception으로 모든 예외 처리하도록 수정 #7

--- app/tests/test_depositwithdrawl.py
+++ app/tests/test_depositwithdrawl.py
@@ -23,34 +23,21 @@ def test_withdraw(setUp):
 def test_deposit_zero(setUp):
     """비정상 입금 테스트(0$ 입금)"""
     controller, acc = setUp
-    result = controller.deposit(acc, 0)
-    assert result is None  
+    with pytest.raises(Exception):
+        controller.deposit(acc, 0)
     assert acc.balance == 30
 
 def test_deposit_negative(setUp):
     """비정상 입금 테스트(음수 입금)"""
     controller, acc = setUp
-    result = controller.deposit(acc, -1)
-    assert result is None  
+    with pytest.raises(Exception):
+        controller.deposit(acc, -1)
     assert acc.balance == 30
 
-def test_withdraw_balance_insufficient(setUp):
-    """비정상 출금 테스트(잔고 부족)"""
+@pytest.mark.parametrize("amount", [40, 0, -1])
+def test_withdraw_invalid(setUp, amount):
+    """비정상 출금 테스트(잔고 부족, 0, 음수)"""
     controller, acc = setUp
-    result = controller.withdraw(acc, 40)
-    assert result is None  
-    assert acc.balance == 30    
-
-def test_withdraw_zero(setUp):
-    """비정상 출금 테스트(0$ 출금)"""
-    controller, acc = setUp
-    result = controller.withdraw(acc, 0)
-    assert result is None  
-    assert acc.balance == 30    
-
-def test_withdraw_negative(setUp):
-    """비정상 출금 테스트(음수$ 출금)"""
-    controller, acc = setUp
-    result = controller.withdraw(acc, -1)
-    assert result is None  
-    assert acc.balance == 30    
+    with pytest.raises(Exception):
+        controller.withdraw(acc, amount)
+    assert acc.balance == 30
