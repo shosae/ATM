0b2162f3e03945eaa05c1a4e7fbd6859de1cf776
shosae <phone13324@gmail.com>
2025-07-23 07:19:35 +0900

    refactor(test): refactor test #1
    - 대규모 리펙토링에 따른 test code 재작성
    - 모든 테스트가 객체가 아닌 ID를 통해 메서드를 호출
    - boolean 비교 시 ==가 아닌 is 사용으로 정확성 높임
    - select_account() 메서드 삭제에 따른 test_selectMultiaccount.py 파일 삭제

--- app/tests/test_integration.py
+++ app/tests/test_integration.py
@@ -13,47 +13,67 @@ def setUp():
 def test_user_flow_insert_card_verify_pin_select_account_deposit_single_account(setUp):
     """계좌가 1개인 카드: 카드 삽입, PIN 인증, 계좌 선택, 입금 통합 테스트"""
     controller = setUp
+    
+    #given
+    card_number = "1234"
+    pin = "4321"
+    acc = "11-11"
+    acc_bal = 30
 
-    # 카드 삽입
-    card = controller.insert_card("1234") # [A1]
+    # 카드 삽입, 등록된 카드인지 검증
+    assert controller.insert_card(card_number)
 
-    # 비밀번호 검증
-    assert card.verify_pin("4321")
+    # PIN 검증
+    assert controller.verify_pin(card_number, pin)
     
     # 계좌 조회
-    accounts = controller.get_accounts_by_card(card)
-    assert len(accounts) == 1
+    res = controller.get_accounts_by_card(card_number)
+    assert res["success"] is True 
+    assert res["data"] == ["11-11"]
     
-    # 계좌 선택
-    acc = controller.select_account(card, 1)
+    # 사용자가 UI에서 조회된 계좌("11-11")를 선택했다고 가정하고 잔액 조회 진행
+    res = controller.get_balance(acc)
+    assert res["success"] is True
+    assert res["data"] == acc_bal
 
-    # 계좌에 입금
-    old_balance = acc.balance
-    controller.deposit(acc, 100)
-    assert acc.balance == old_balance + 100
+    # 입금 진행
+    res = controller.deposit(acc, 100)
+    assert res["success"] is True 
+    assert res["data"] == acc_bal + 100
+    
 
 def test_user_flow_insert_card_verify_pin_select_account_withdraw_deposit_multi_account(setUp):
     """계좌가 여러 개인 카드: 카드 삽입, PIN 인증, 계좌 선택, 입금 및 출금 통합 테스트"""
     controller = setUp
 
-    # 카드 삽입
-    card = controller.insert_card("2345") # [A2, A3]
+    #given
+    card = "2345"
+    pin = "5432"
+    acc1 = "22-22"
+    acc2 = "33-33"
+    acc1_bal = 500
+    acc2_bal = 1000
+
+    # 카드 삽입, 등록 검증
+    assert controller.insert_card(card)
     
-    # 비밀번호 검증
-    assert card.verify_pin("5432")
+    # PIN 검증
+    assert controller.verify_pin(card, pin)
     
     # 계좌 조회
-    accounts = controller.get_accounts_by_card(card)
-    assert len(accounts) > 1
-    
-    # 첫 번째 계좌에 입금
-    acc1 = controller.select_account(card, 1)
-    old_balance1 = acc1.balance
-    controller.deposit(acc1, 100)
-    assert acc1.balance == old_balance1 + 100
+    res = controller.get_accounts_by_card(card)
+    assert res["success"] is True
+    assert len(res["data"]) == 2
 
-    # 두 번째 계좌에 출금
-    acc2 = controller.select_account(card, 2)
-    old_balance2 = acc2.balance
-    controller.withdraw(acc2, 200)
-    assert acc2.balance == old_balance2 - 200
+    # 사용자가 UI에서 첫 번째 계좌("22-22")를 선택해 입금,
+    # 두 번째 계좌("33-33")를 선택해 출금하는 상황을 가정
+
+    # 첫 번째 계좌("22-22")에 입금
+    res = controller.deposit(acc1, 100)
+    assert res["success"] is True
+    assert res["data"] == acc1_bal + 100
+
+    # 두 번째 계좌("33-33")에 출금
+    res = controller.withdraw(acc2, 200)
+    assert res["success"] is True
+    assert res["data"] == acc2_bal - 200
