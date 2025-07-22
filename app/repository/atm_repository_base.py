20ae5f2ad35ffe338abeafc68990027936353f34
shosae <phone13324@gmail.com>
2025-07-23 06:58:08 +0900

    refactor(repository): refactor repository layer #1
    - 모든 메서드에 대해 객체가 아닌 ID를 기반으로 데이터를 조회하도록 변경
    - 초기 데이터 구조에서도 카드 객체가 계좌 객체가 아닌 계좌 번호 목록을 갖도록 수정
    - ATMRepository 타입 힌트 일부 수정

--- app/repository/atm_repository_base.py
+++ app/repository/atm_repository_base.py
@@ -1,13 +1,15 @@
 from abc import ABC, abstractmethod
-from typing import Optional, List
+from typing import Optional
 from domain.card import Card
 from domain.account import Account
 
 class ATMRepository(ABC):
     @abstractmethod
-    def find_card_by_number(self, card_number) -> Optional[Card]:
+    def find_card_by_number(self, card_number: str) -> Optional[Card]:
+        """카드 번호로 카드 객체 반환, 없으면 None 반환"""
         pass
 
     @abstractmethod
-    def get_card_accounts(self, card_number) -> List[Account]:
+    def find_account_by_number(self, account_number: str) -> Optional[Account]:
+        """계좌 번호로 계좌 객체 반환, 없으면 None 반환"""
         pass
